9eff19f3d8608d6fc9b8179d6b00d951d84986b9
ShreyaBarsude02 <142074907+ShreyaBarsude02@users.noreply.github.com>
2025-02-21 13:52:51 +0530

    Update checking_frame_shape.py

--- checking_frame_shape.py
+++ checking_frame_shape.py
@@ -1,4 +1,8 @@
 from picamera2 import Picamera2
+
 picam2 = Picamera2()
-config = picam2.create_still_configuration()
-print(config["size"])  # Outputs (width, height)
+config = picam2.create_preview_configuration()  # Use preview config
+picam2.configure(config)
+
+print("Frame width:", config["main"]["size"][0])
+print("Frame height:", config["main"]["size"][1])
