ca3b564e9872acc9c595f936807cd254bb0333f5
ShreyaBarsude02 <142074907+ShreyaBarsude02@users.noreply.github.com>
2025-02-21 13:50:06 +0530

    Update checking_frame_shape.py

--- checking_frame_shape.py
+++ checking_frame_shape.py
@@ -1,15 +1,4 @@
-import cv2
-
-cap = cv2.VideoCapture(0)  # Use 0 for the default camera
-
-if not cap.isOpened():
-    print("Error: Could not open camera.")
-else:
-    ret, frame = cap.read()
-    if ret:
-        height, width, _ = frame.shape
-        print(f"Frame Width: {width}, Frame Height: {height}")
-    else:
-        print("Error: Could not read frame.")
-
-cap.release()
+from picamera2 import Picamera2
+picam2 = Picamera2()
+config = picam2.create_still_configuration()
+print(config["size"])  # Outputs (width, height)
